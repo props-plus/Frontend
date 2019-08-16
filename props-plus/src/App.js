9853b592146d9a6d125a76c9351a7528abccfd51
Dionuta <diounta@gmail.com>
2019-08-15 23:59:23 -0700

    Junk that we don't need has been deleted.

---
+++ props-plus/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Soon...</h1>
+      <p>Be patient, it's coming...</p>
+    </div>
+  );
+}
+
+export default App;
