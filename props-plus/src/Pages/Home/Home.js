3cba6459dbc9264b21b87bb492120200dad66530
Dionuta <diounta@gmail.com>
2019-08-21 01:51:27 -0700

    Ready to go live.

--- props-plus/src/Pages/Home/Home.js
+++ props-plus/src/Pages/Home/Home.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+
+class Home extends Component {
 
 
-function Home() {
-  return (
-    // Basic coming soon message.
-    <div className="App"> 
-      <h1>Soon...</h1>
-      <p>Be patient, it's coming.....</p>
-    </div>
-  );
+  initializeReactGA() {
+    ReactGA.initialize("UA-146126284-1"); //Tracking ID
+    ReactGA.pageview("/homepage"); //Name of the page being
+  }
+
+  componentDidMount() {
+      this.initializeReactGA(); //When ever componentDidMount event is triggered.
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Soon...</h1>
+        <p>Be patient, it's coming.....</p>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- props-plus/src/index.js
+++ props-plus/src/index.js
@@ -2,7 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import 'babel-polyfill';
 
 import './index.css';
 import App from './App';
