75d8f2175e7e9b326dd9b9aeed544e65a3d8acfb
justinandrade <justinandrade@github.com>
2019-08-24 14:30:37 -0400

    Auth0 implemented into front-end, as-well as nav Login button

--- props-plus/src/Pages/Landing-Page/Nav/Nav.js
+++ props-plus/src/Pages/Landing-Page/Nav/Nav.js
@@ -1,49 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactGA from "react-ga";
 
 import { Link } from "react-router-dom";
 
-import {NavStyle} from "./styles"
+import { NavStyle } from "./styles";
 
+import Login from "../../../Components/Auth0/Login.js";
 
-const routes = [ 
-  { path: '/', name: 'Home' },
-  { path: '/about', name: 'About' },
-  { path: '/pricing', name: "Pricing"}
-]
+function Nav() {
+  const routes = [
+    { path: "/", name: "Home" },
+    { path: "/about", name: "About" },
+    { path: "/pricing", name: "Pricing" }
+  ];
 
-
-class Nav extends Component {
-  
-
-  render() {
-    return (
-     <NavStyle>
-        <style>
-          @import url('https://fonts.googleapis.com/css?family=Oswald');
-        </style>
-        <style>
-          @import url('https://fonts.googleapis.com/css?family=Indie+Flower');
-        </style>
-        <div className="top-bar">
-          <div className="container">
-            
-            <div className="container-center">
-              {routes.map(route => (
-              <Link
-                key={route.path}
-                to={route.path}
-                
-              >
+  return (
+    <NavStyle>
+      <style>
+        @import url('https://fonts.googleapis.com/css?family=Oswald');
+      </style>
+      <style>
+        @import url('https://fonts.googleapis.com/css?family=Indie+Flower');
+      </style>
+      <div className="top-bar">
+        <div className="container">
+          <div className="container-center">
+            {routes.map(route => (
+              <Link key={route.path} to={route.path}>
                 {route.name}
               </Link>
             ))}
-            </div>
+            <Login />
           </div>
         </div>
-      </NavStyle>
-    );
-  }
+      </div>
+    </NavStyle>
+  );
 }
 
 export default Nav;
